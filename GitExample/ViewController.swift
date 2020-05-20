7cc942793975901b1e1b0bb481d4f418beedf58c
Trilok <tmunagala@steadyapp.com>
2020-05-20 15:14:06 +0530

    Initial Commit

---
+++ GitExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitExample
+//
+//  Created by Trilok on 5/20/20.
+//  Copyright © 2020 datapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
